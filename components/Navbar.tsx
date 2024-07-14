fa4cabbf0d7629ffa51156d7f66973cb515af349
afisman <afisman@gmail.com>
2024-07-14 21:11:07 +0200

    worked on nav

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <header className='w-full'>
+            <nav className='nav'>
+                Navbar
+            </nav>
+        </header>
+
+    )
+}
+
+export default Navbar
