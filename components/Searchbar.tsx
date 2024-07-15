ce3b551d6fd676c53c3af3955595545472a94e01
afisman <afisman@gmail.com>
2024-07-15 13:14:27 +0200

    worked on carousel

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,8 +1,25 @@
+"use client"
+
 import React from 'react';
 
 const Searchbar = () => {
+
+    const handleSubmit = () => { }
     return (
-        <div>Searchbar</div>
+        <form
+            className='flex flex-wrap gap-4 mt-12'
+            onSubmit={handleSubmit}
+        >
+            <input
+                type='text'
+                placeholder='Enter product link'
+                className='searchbar-input'
+            />
+
+            <button type='submit' className='searchbar-btn'>
+                Search
+            </button>
+        </form>
     )
 }
 
